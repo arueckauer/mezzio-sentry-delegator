6468c624b6b78b73c4cb965708e3f2493739721c
Andi Rückauer <arueckauer@gmail.com>
2021-09-21 16:04:08 +0200

    Add ErrorListener

---
+++ src/MezzioSentryDelegator/ErrorListener.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MezzioSentryDelegator;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Throwable;
+
+use function Sentry\captureException;
+
+class ErrorListener
+{
+    public function __invoke(Throwable $exception, ServerRequestInterface $request, ResponseInterface $response): void
+    {
+        captureException($exception);
+    }
+}
